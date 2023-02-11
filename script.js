ea577ea23832165210d74b4497c850887566ea2b
Mohammed Ali <84571198+MohammedAli996@users.noreply.github.com>
2023-02-11 23:43:32 +0100

    navbar + css for navbar

---
+++ script.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      cartToggle = document.querySelector(".cartToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+
+// js code to toggle cart box
+        cartToggle.addEventListener("click" , () =>{
+        cartToggle.classList.toggle("active");
+      });
+ 
+      
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click" , () =>{
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
+});
